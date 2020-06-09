06aad13c9efae42a03188bb7ddc337958927a5c8
Fizza Rubab <fr06161@st.habib.edu.pk>
2020-06-10 01:12:45 +0500

    Commented Code. Few changes made. Added a plot.

--- Code/math_roulettewheel.py
+++ Code/math_roulettewheel.py
@@ -1,6 +1,32 @@
-
 import random
+from mpl_toolkits import mplot3d
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+################################# CODE FOR THE 3D  PLOT ##########################
 
+
+def math_function(x, y):
+    return np.sin(np.sqrt(x ** 2 + y ** 2))
+
+x = np.linspace(-6, 6, 30)
+y = np.linspace(-6, 6, 30)
+
+X, Y = np.meshgrid(x, y)
+Z = math_function(X, Y)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X, Y, Z, 50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show()
+##############################################################################33
+
+#Generate random solutions {x,y} within the domain of the function 
+#and adds them to population
 def generate_population(size, x_boundaries, y_boundaries):
     lower_x_boundary, upper_x_boundary = x_boundaries
     lower_y_boundary, upper_y_boundary = y_boundaries
@@ -15,38 +41,38 @@ def generate_population(size, x_boundaries, y_boundaries):
 
     return population
 
-import math
-
+################ OBJECTIVE FUNCTION WE WANT TO MAXIMIZE ##################
 def apply_function(individual):
     x = individual["x"]
     y = individual["y"]
-    
-    return (x ** 2 + y ** 2)
+    return math.sin(math.sqrt(x ** 2 + y ** 2))
 
+################## SELECTION STRATEGY TO FORM A PINWHEEL AND SELECT A RANDOM NUMBER
 def choice_by_roulette(sorted_population, fitness_sum):
     offset = 0
-    normalized_fitness_sum = fitness_sum
+    normalized_fitness_sum = fitness_sum #total fitness
 
     lowest_fitness = apply_function(sorted_population[0])
     if lowest_fitness < 0:
         offset = -lowest_fitness
         normalized_fitness_sum += offset * len(sorted_population)
 
-    draw = random.uniform(0, 1)
+    draw = random.uniform(0, 1) #random number
 
     accumulated = 0
     for individual in sorted_population:
         fitness = apply_function(individual) + offset
-        probability = fitness / normalized_fitness_sum
+        probability = fitness / normalized_fitness_sum  #fitness ratio
         accumulated += probability
 
         if draw <= accumulated:
             return individual
 
+##### sprt population in ascending order of their fitness levels        
 def sort_population_by_fitness(population):
     return sorted(population, key=apply_function)
 
-
+######## crossover by taking a point in between the two coordinates ###########
 def crossover(individual_a, individual_b):
     xa = individual_a["x"]
     ya = individual_a["y"]
@@ -56,10 +82,10 @@ def crossover(individual_a, individual_b):
 
     return {"x": (xa + xb) / 2, "y": (ya + yb) / 2}
 
-
+########### mutate witihin these bounds
 def mutate(individual):
-    next_x = individual["x"] + random.uniform(-0.1,0.3)
-    next_y = individual["y"] + random.uniform(-0.1, 0.3)
+    next_x = individual["x"] + random.uniform(-0.05, 0.05)
+    next_y = individual["y"] + random.uniform(-0.05, 0.05)
 
     lower_boundary, upper_boundary = (-4, 4)
 
@@ -85,21 +111,20 @@ def make_next_generation(previous_population):
         next_generation.append(individual)
 
     return next_generation
-#So this is it! We now have all the three steps of a GA: selection, crossover and mutation. Our main method is then simply like that:
+
 generations = 400
 
-population = generate_population(size=5, x_boundaries=(-4, 4), y_boundaries=(-4, 4))
-
+population = generate_population(size=10, x_boundaries=(-4, 4), y_boundaries=(-4, 4))
+progress=[]
 i = 1
 while True:
     print(f" GENERATION {i}")
 
     for individual in population:
         print(individual, apply_function(individual))
-
+    progress.append(apply_function(sort_population_by_fitness(population)[-1]))
     if i == generations:
         break
-
     i += 1
 
     population = make_next_generation(population)
@@ -107,3 +132,7 @@ while True:
 best_individual = sort_population_by_fitness(population)[-1]
 print("\n FINAL RESULT")
 print(best_individual, apply_function(best_individual))
+plt.plot(progress)
+plt.ylabel('Fitness Score')
+plt.xlabel('Generation')
+plt.show()
